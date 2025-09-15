2f943fe591b132514169a053cbde795a99ebc706
Nguyen Dinh Khoi Nguyen <hpkhoinguyen@gmail.com>
2025-09-15 12:35:39 +0700

    Using ORM to edit data into Database Update/(CRUD) Part_3

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -29,14 +29,38 @@ let displayGetCRUD = async (req, res) => {
   console.log("===================");
   console.log(data);
   console.log("===================");
+
   return res.render("displayCRUD.ejs", {
     dataTable: data,
   });
 };
+let getEditCRUD = async (req, res) => {
+  let userId = req.query.id;
+  if (userId) {
+    let userData = await CRUDService.getUserInfoById(userId);
+    // check user data not found
+
+    return res.render("editCRUD.ejs", {
+      user: userData,
+    });
+  } else {
+    return res.send("User not found!");
+  }
+};
+let putCRUD = async (req, res) => {
+  let data = req.body;
+  let allUser = await CRUDService.updateUserData(data);
+  return res.render("displayCRUD.ejs", {
+    dataTable: allUser,
+  });
+};
+
 module.exports = {
   getHomePage: getHomePage,
   getAboutPage: getAboutPage,
   getCRUD: getCRUD,
   postCRUD: postCRUD,
   displayGetCRUD: displayGetCRUD,
+  getEditCRUD: getEditCRUD,
+  putCRUD: putCRUD,
 };
