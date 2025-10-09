c75b63aeafec29952c8c183e6aeb709a4408e88d
Nguyen Dinh Khoi Nguyen <hpkhoinguyen@gmail.com>
2025-10-09 22:42:55 +0700

    complete Menu UI grant for Cho Admin

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -13,7 +13,6 @@ class HomeHeader extends Component {
   };
   render() {
     let language = this.props.language;
-    console.log("check language:", language);
     return (
       <React.Fragment>
         <div className="home-header-container">
@@ -169,6 +168,7 @@ class HomeHeader extends Component {
 const mapStateToProps = (state) => {
   return {
     isLoggedIn: state.user.isLoggedIn,
+    userInfo: state.user.userInfo,
     language: state.app.language,
     // redux inject into react
   };
