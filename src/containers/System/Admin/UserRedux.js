bc944460e74e5b20596468e8b965dcbfe35487ae
Nguyen Dinh Khoi Nguyen <hpkhoinguyen@gmail.com>
2025-11-01 13:56:04 +0700

    WRITE & READ FILE IMAGE với Sequelize và MySQL BLOB

--- src/containers/System/Admin/UserRedux.js
+++ src/containers/System/Admin/UserRedux.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { FormattedMessage } from "react-intl";
 import { connect } from "react-redux";
-import { LANGUAGES, CRUD_ACTIONS } from "../../../utils";
+import { LANGUAGES, CRUD_ACTIONS, CommonUtils } from "../../../utils";
 import * as actions from "../../../store/actions";
 import "./UserRedux.scss";
 import Lightbox from "react-image-lightbox";
@@ -95,16 +95,21 @@ class UserRedux extends Component {
         role: arrRoles && arrRoles.length > 0 ? arrRoles[0].key : "",
         avatar: "",
         action: CRUD_ACTIONS.CREATE,
+        previewImgURL: "",
       });
       console.log("componentDidUpdate:", this.state);
     }
   }
-  handleOnChangeImage = (event) => {
+  handleOnChangeImage = async (event) => {
     let data = event.target.files;
     let file = data[0];
     if (file) {
+      let base64 = await CommonUtils.getBase64(file);
       let objectURL = URL.createObjectURL(file);
-      this.setState({ previewImgURL: objectURL, avatar: file });
+      this.setState({
+        previewImgURL: objectURL,
+        avatar: base64,
+      });
     }
   };
 
@@ -131,6 +136,7 @@ class UserRedux extends Component {
         gender: this.state.gender,
         roleId: this.state.role,
         positionId: this.state.position,
+        avatar: this.state.avatar,
       });
     }
     if (action === CRUD_ACTIONS.EDIT) {
@@ -147,6 +153,7 @@ class UserRedux extends Component {
         gender: this.state.gender,
         roleId: this.state.role,
         positionId: this.state.position,
+        avatar: this.state.avatar,
       });
     }
   };
@@ -180,6 +187,11 @@ class UserRedux extends Component {
   };
 
   handleEditUserFromParent = (user) => {
+    let imageBase64 = "";
+    if (user.image) {
+      // how to convert from base64 database to image (edit)
+      imageBase64 = new Buffer(user.image, "base64").toString("binary");
+    }
     this.setState({
       email: user.email,
       password: "HARDCODE",
@@ -191,6 +203,7 @@ class UserRedux extends Component {
       position: user.positionId,
       role: user.roleId,
       avatar: "",
+      previewImgURL: imageBase64,
       action: CRUD_ACTIONS.EDIT,
       userEditId: user.id,
     });
